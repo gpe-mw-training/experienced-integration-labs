d48d0b255b72a5f8339667a585bf5340263c26cb
Khaled Janania <kjanania@redhat.com>
2018-09-25 09:47:58 -0400

    Have the processor working to convert the objects

--- code/01_file-split-and-transform/routing/src/main/java/org/acme/routes/CustomerRoute.java
+++ code/01_file-split-and-transform/routing/src/main/java/org/acme/routes/CustomerRoute.java
@@ -1,8 +1,10 @@
 package org.acme.routes;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.language.ExpressionDefinition;
 import org.springframework.stereotype.Component;
 import org.globex.Account;
@@ -14,7 +16,8 @@ import org.acme.Customer;
 public class CustomerRoute extends RouteBuilder {
     @Override
     public void configure () throws Exception {
-        onException(IllegalArgumentException.class).to("log:fail").handled(true).stop();
+//        getContext().getProperties().put("CamelJacksonEnableTypeConverter", "true");
+        onException(IllegalArgumentException.class).to("log:fail").to("file://src/data/error?fileName=csv-record-${date:now:yyyyMMdd}.txt").handled(true).stop();
 //        from("timer:foo").to("log:bar");
         BindyCsvDataFormat format = new BindyCsvDataFormat(org.acme.Customer.class);
         format.setLocale("default");
@@ -22,7 +25,10 @@ public class CustomerRoute extends RouteBuilder {
             .split().tokenize("\n")
             .to("log:bar")
             .unmarshal(format).to("log:foo")
-            .transform().expression(new CustomerTransformer()).to("log:tranformed");
+            .process(new CustomerProcessor())
+            .to("log:transformed")
+            .marshal().json(JsonLibrary.Jackson).to("file://src/data/outbox?fileName=account-${property.CamelSplitIndex}.json");
+//            .transform().expression(new CustomerTransformer()).to("log:tranformed");
 //            .unmarshal(format);
 //                .transform().
 ////                .json()
@@ -30,9 +36,35 @@ public class CustomerRoute extends RouteBuilder {
 //                onException(IllegalArgumentException.class).to("file://src/data/error?fileName=csv-record-${date:now:yyyyMMdd}.txt");
     }
 
+    class CustomerProcessor implements Processor {
+        public void process(Exchange exchange) throws Exception {
+            Customer customer = exchange.getIn().getBody(Customer.class);
+            Account theAccount = new Account();
+            Company theCompany = new Company();
+            Contact theContact = new Contact();
+
+            theContact.setCity(customer.getCity());
+            theContact.setFirstName(customer.getFirstName());
+            theContact.setLastName(customer.getLastName());
+            theContact.setPhone(customer.getPhone());
+            theContact.setState(customer.getState());
+            theContact.setStreetAddr(customer.getStreetAddr());
+            theContact.setZip(customer.getZip());
+
+            theCompany.setActive(customer.isActive());
+            theCompany.setGeo(customer.getRegion());
+            theCompany.setName(customer.getCompanyName());
+
+            theAccount.setCompany(theCompany);
+            theAccount.setContact(theContact);
+
+            exchange.getIn().setBody(theAccount, Account.class);
+        }
+    }
+
     class CustomerTransformer extends ExpressionDefinition {
         Account evaluate(Exchange exchange, Account account) {
-            Customer customer = exchange.getIn(Customer.class);
+            Customer customer = exchange.getIn().getBody(Customer.class);
             Account theAccount = new Account();
             Company theCompany = new Company();
             Contact theContact = new Contact();
