6080b8df92e7b693d3a362a35f4a8b121cf67bde
Khaled Janania <kjanania@redhat.com>
2018-10-05 12:00:59 -0400

    Cleaning up tests and poms

--- code/01_file-split-and-transform-sb/routing/src/main/java/org/acme/routes/CustomerRoute.java
+++ code/01_file-split-and-transform-sb/routing/src/main/java/org/acme/routes/CustomerRoute.java
@@ -8,7 +8,6 @@ import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.language.ExpressionDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.globex.Account;
 import org.globex.Company;
