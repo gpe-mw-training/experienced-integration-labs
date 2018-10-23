18ed75efe46f78783638337ea531d5ef893e1be6
Nandan Joshi <njoshi@redhat.com>
2018-10-23 11:33:46 -0700

    added Application.java

---
+++ code/02_enrich-content-REST-and-WS-sb/routing/src/main/java/org/acme/Application.java
@@ -0,0 +1,11 @@
+package org.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String... args) {
+        SpringApplication.run(org.acme.Application.class, args);
+    }
+}
